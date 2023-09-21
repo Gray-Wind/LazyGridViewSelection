3df05f194ed073fcb8c15f60c619e9b0fb60dd60
Ilia Kolo <ilia.kolo@alludo.com>
2023-09-21 17:43:04 +0300

    Initial Commit

---
+++ LazyGridViewSelection/LazyGridViewSelectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyGridViewSelectionApp.swift
+//  LazyGridViewSelection
+//
+//  Created by Ilia Kolomeitsev on 21.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct LazyGridViewSelectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
